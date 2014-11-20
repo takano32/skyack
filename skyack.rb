9d06c79cb4fe580d8165bcbf9fcb67512210abf6
TAKANO Mitsuhiro <takano32@gmail.com>
2014-11-20 14:06:32 +0900

    add skyack.rb

---
+++ skyack.rb
@@ -0,0 +1,25 @@
+config = {
+  'team'           => 'skyack'
+  'channel'        => '#general',
+  'name'           => 'skyack',
+  'incoming_token' => 'secret',
+  'outgoing_token' => 'secret'
+}
+
+bot = Slackbotsy::Bot.new(config) do
+
+  hear /echo\s+(.+)/ do |data, mdata|
+    "I heard #{data['user_name']} say '#{mdata[1]}' in #{data['channel_name']}"
+  end
+
+  hear /flip out/i do
+    open('http://tableflipper.com/gif') do |f|
+      "<#{f.read}>"
+    end
+  end
+
+end
+
+post '/' do
+  bot.handle_item(params)
+end
